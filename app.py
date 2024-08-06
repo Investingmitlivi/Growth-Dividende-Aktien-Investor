4cd054fabf69ec03ae35bb77c6d076a38c4ee2b5
Investingmitlivi <139369847+Investingmitlivi@users.noreply.github.com>
2024-08-06 18:40:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7394,8 +7394,10 @@ if selected == "Stock Analysis Tool":
 
     
 
-     #url = f'https://public-api.quickfs.net/v1/data/all-data/{ticker}?api_key={api_key}'
-     url = f'https://public-api.quickfs.net/v1/data/all-data/{ticker}?api_key={os.getenv('api_key')}'
+     url = f'https://public-api.quickfs.net/v1/data/all-data/{ticker}?api_key={api_key}'
+     #url = f'https://public-api.quickfs.net/v1/data/all-data/{ticker}?api_key={os.getenv('api_key')}'
+     #url = f'https://public-api.quickfs.net/v1/data/all-data/{ticker}?api_key={os.getenv('api_key')}'
+
      #usage_url = f'https://public-api.quickfs.net/v1/usage'
     
 
